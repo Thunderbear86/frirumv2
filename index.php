3de7a899a49a7fd85629ab737b601267649f78d5
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-05-29 22:27:32 +0200

    Ny landingpage - update

--- index.php
+++ index.php
@@ -50,40 +50,42 @@
     </div>
 
 
-<div class="container">
+<div class="container mt-3">
     <div class="row">
         <div class="col-sm-6 col-md-3 col-lg-3">
-            <h3 class="text-center">Åbningstider</h3>
-            <ul>
-                <lo>Mandag</lo>
-                <lo>Tirsdag</lo>
-            </ul>
+            <h3>Åbningstider</h3>
+                <p class="mb-0">Mandag</p>
+                <p class="mb-0">Tirsdag</p>
+                <p class="mb-0">Onsdag</p>
+                <p class="mb-0">Torsdag</p>
+                <p class="mb-0">Fredag</p>
+                <p class="mb-0">Lørdag</p>
+                <p class="mb-0">Søndag</p>
         </div>
         <div class="col-sm-6 col-md-3 col-lg-3">
-            <h3 class="text-center">Info</h3>
-            <ul>
-                <a>Om FriRUM</a>
-                <a>Kontakt</a>
-                <a>FAQ</a>
-                <a>Nyhedsbrev</a>
-                <a>Handelsbetingelser</a>
-                <a>Privatlivspolitik</a>
-            </ul>
+            <h3>Info</h3>
+            <p class="mb-0"><a>Om FriRUM</a></p>
+            <p class="mb-0"><a>Kontakt</a></p>
+            <p class="mb-0"><a>FAQ</a></p>
+            <p class="mb-0"><a>Nyhedsbrev</a></p>
+            <p class="mb-0"><a>Handelsbetingelser</a></p>
+            <p class="mb-0"><a>Privatlivspolitik</a></p>
         </div>
-        <div class="col-sm-6 col-md-3 col-lg-3">
-            <h3 class="text-center">Adresse</h3>
-            <div class="align-content-center">
-                <ul>
-                    <lo>Adresse 10 ST</lo>
-                    <lo>4200 Slagelse</lo>
-                    <br>
-                    <lo>CVR</lo>
-                    <lo>Hoved Mail</lo>
-                </ul>
+            <div class="col-sm-6 col-md-3 col-lg-3">
+                <div>
+                    <h3>Adresse</h3>
+                        <p class="mb-0">Adresse 10 ST</p>
+                        <p class="mb-0">4200 Slagelse</p>
+                        <br>
+                        <p class="mb-0">CVR</p>
+                        <p class="mb-0">Hoved Mail</p>
+                </div>
             </div>
-        </div>
-        <div class="col-sm-6 col-md-3 col-lg-3">
-            <img src="img/paere.jpg" alt="Logo" class="rounded-circle logo-img g-2 logo-style" style="width: 65px; height: 60px">
+            <div class="col-sm-6 col-md-3 col-lg-3">
+                <a class="navbar-brand me-3 p-2" href="index.php">
+                    <img src="img/paere.jpg" alt="Logo" class="rounded-circle logo-img g-2 logo-style" style="width: 65px; height: 60px">
+                </a>
+            </div>
         </div>
     </div>
 </div>
