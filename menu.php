cab88a3436a721ffd5264553bf86d196ec20afcb
Thunderbear86 <116293845+Thunderbear86@users.noreply.github.com>
2023-06-12 07:33:48 +0200

    Menu items center mx-auto fix

--- menu.php
+++ menu.php
@@ -2,44 +2,40 @@
 
 <div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col">
-            <nav class="navbar navbar-expand-lg bg-dark fixed-top">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <i class="fas fa-bars fa-2x navbar-button"></i>
-                    </button> 
-                    
-          
-                
-                <div class="d-flex align-items-center col">
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav p-3">
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="index.php">Hjem</a>
-                            </li>
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="kalender.php">Kalender</a>
-                            </li>
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="fstv.php">FriRUM Stuen TV</a>
-                            </li>
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="kreakassen.php">KreaKASSEN</a>
-                            </li>
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="#">Workshops</a>
-                            </li>
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="#">Teambuilding</a>
-                            </li>
-                            <li class="nav-item me-3">
-                                <a class="nav-link menu-text" href="om.php">Om FriRUM Lab</a>
-                            </li>
-                        </ul>
-                    </div>    
-                </div>
-            </nav>
-        </div>
+        <nav class="navbar navbar-expand-lg bg-dark fixed-top">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars fa-2x navbar-button"></i>
+            </button> 
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto p-3">
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="index.php">Hjem</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="kalender.php">Kalender</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="fstv.php">FriRUM Stuen TV</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="kreakassen.php">KreaKASSEN</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="#">Workshops</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="#">Teambuilding</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link menu-text" href="om.php">Om FriRUM Lab</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </div>
 </div>
+
+
 ' ; ?>
 
